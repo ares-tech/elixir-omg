d39c4bb0a4106c38b187e25104193bfb8c475220
Paul Peregud <paulperegud@gmail.com>
2018-12-07 11:42:37 +0100

    fix: leveldb ife types mismatch problem

--- apps/omg_db/lib/leveldb_server.ex
+++ apps/omg_db/lib/leveldb_server.ex
@@ -76,24 +76,12 @@ defmodule OMG.DB.LevelDBServer do
   end
 
   def handle_call({:utxos}, _from, %__MODULE__{db_ref: db_ref} = state) do
-    result =
-      db_ref
-      |> Exleveldb.stream()
-      |> LevelDBCore.filter_utxos()
-      |> Enum.map(fn {_, value} -> {:ok, value} end)
-      |> LevelDBCore.decode_values(:utxo)
-
+    result  = get_by_type(:utxo, db_ref)
     {:reply, result, state}
   end
 
   def handle_call({:exit_infos}, _from, %__MODULE__{db_ref: db_ref} = state) do
-    result =
-      db_ref
-      |> Exleveldb.stream()
-      |> LevelDBCore.filter_exit_infos()
-      |> Enum.map(fn {_, value} -> {:ok, value} end)
-      |> LevelDBCore.decode_values(:exit_info)
-
+    result  = get_by_type(:exit_info, db_ref)
     {:reply, result, state}
   end
 
@@ -108,14 +96,13 @@ defmodule OMG.DB.LevelDBServer do
   end
 
   def handle_call({:in_flight_exits_info}, _from, %__MODULE__{db_ref: db_ref} = state) do
-    result =
-      db_ref
-      |> Exleveldb.stream()
-      |> LevelDBCore.filter_in_flight_exits_info()
-      |> Enum.map(fn {_, value} -> {:ok, value} end)
-      |> LevelDBCore.decode_values(:in_flight_exit_info)
+    result = get_by_type(:in_flight_exit_info, db_ref)
+    {:reply, result, state}
+  end
 
-    {:reply, {:ok, []}, state}
+  def handle_call({:competitors_info}, _from, %__MODULE__{db_ref: db_ref} = state) do
+    result  = get_by_type(:competitor_info, db_ref)
+    {:reply, result, state}
   end
 
   def handle_call(parameter, _from, %__MODULE__{db_ref: db_ref} = state)
@@ -143,4 +130,13 @@ defmodule OMG.DB.LevelDBServer do
   defp get(key, db_ref) do
     Exleveldb.get(db_ref, key)
   end
+
+  defp get_by_type(type, db_ref) do
+    db_ref
+    |> Exleveldb.stream()
+    |> LevelDBCore.filter_keys(type)
+    |> Enum.map(fn {_, value} -> {:ok, value} end)
+    |> LevelDBCore.decode_values(type)
+  end
+
 end
