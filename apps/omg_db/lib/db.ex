5431836e2f80b933e113ca925fac123d81fdb98f
Pawel Nowosielski <Pawel.Nowosielski@imapp.pl>
2018-12-19 16:15:27 +0100

    feat: create challenge baked by OMG.DB data

--- apps/omg_db/lib/db.ex
+++ apps/omg_db/lib/db.ex
@@ -51,6 +51,11 @@ defmodule OMG.DB do
     GenServer.call(server_name, {:exit_infos}, timeout_ms)
   end
 
+  @spec spent_blknum({pos_integer, non_neg_integer, non_neg_integer}, atom) :: {:ok, pos_integer} | {:error, atom}
+  def spent_blknum(utxo_pos, server_name \\ @server_name) do
+    GenServer.call(server_name, {:spent_blknum, utxo_pos})
+  end
+
   def block_hashes(block_numbers_to_fetch, server_name \\ @server_name) do
     GenServer.call(server_name, {:block_hashes, block_numbers_to_fetch})
   end
